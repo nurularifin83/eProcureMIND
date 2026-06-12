a332a3c8d4bf2f2f2342f1f51e782a8d2227ee8e
Nurul Arifin <nurularifin837@gmail.com>
2026-06-13 01:48:44 +0700

    Update node version

--- fixtures/users.ts
+++ fixtures/users.ts
@@ -1,51 +1,42 @@
 export const users = {
   admin: {
-    username: 'admin',
-    password: 'kulonuwon'
+    username: process.env.ADMIN_USERNAME!,
+    password: process.env.ADMIN_PASSWORD!,
   },
-
   vendor: {
-    username: '...',
-    password: '...'
+    username: process.env.VENDOR_USERNAME!,
+    password: process.env.VENDOR_PASSWORD!,
   },
-
   HSESpecialist: {
-    username: 'azzam.syahid',
-    password: 'procsijay@1'
+    username: process.env.HSE_USERNAME!,
+    password: process.env.HSE_PASSWORD!,
   },
-
   adminSCM: {
-    username: 'dessy.claudia',
-    password: 'procsijay@1'
+    username: process.env.ADMIN_SCM_USERNAME!,
+    password: process.env.ADMIN_SCM_PASSWORD!,
   },
-
   VML: {
-    username: 'mogi.aria',
-    password: 'procsijay@1'
+    username: process.env.VML_USERNAME!,
+    password: process.env.VML_PASSWORD!,
   },
-
   HeadSCM: {
-    username: 'proid.marta',
-    password: 'procsijay@1'
+    username: process.env.HEAD_SCM_USERNAME!,
+    password: process.env.HEAD_SCM_PASSWORD!,
   },
-
   creatorProcplan: {
-    username: 'yosua.situmorang',
-    password: 'procsijay@1'
+    username: process.env.CREATOR_PROCPLAN_USERNAME!,
+    password: process.env.CREATOR_PROCPLAN_PASSWORD!,
   },
-
   requesterProcplan: {
-    username: 'marsha.tolla',
-    password: 'procsijay@1'
+    username: process.env.REQUESTER_PROCPLAN_USERNAME!,
+    password: process.env.REQUESTER_PROCPLAN_PASSWORD!,
   },
-
   confirmerProcplan: {
-    username: 'perdana.saputro',
-    password: 'procsijay@1'
+    username: process.env.CONFIRMER_PROCPLAN_USERNAME!,
+    password: process.env.CONFIRMER_PROCPLAN_PASSWORD!,
   },
-
   reviewerProcplan: {
-    username: 'yusfi.iskandar',
-    password: 'procsijay@1'
-  }
+    username: process.env.REVIEWER_PROCPLAN_USERNAME!,
+    password: process.env.REVIEWER_PROCPLAN_PASSWORD!,
+  },
 }
