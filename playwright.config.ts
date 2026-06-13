4c438074acac91c8277ab8736ce71e7ff2435997
Nurul Arifin <nurularifin837@gmail.com>
2026-06-13 11:22:13 +0700

    Update .yml

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  /* reporter: 'html', */
+  reporter: [
+    ['html'],
+    ['allure-playwright', {
+      detail: true,
+      outputFolder: 'allure-results',
+      suiteTitle: false,
+    }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -47,15 +55,23 @@ export default defineConfig({
         dependencies: ['setup'],
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',                
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //     storageState: 'auth/admin.json',
+    //   },
+    //   dependencies: ['setup'],
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: {
+    //     ...devices['Desktop Safari'],
+    //     storageState: 'auth/admin.json',
+    //   },
+    //   dependencies: ['setup'],
+    // },
 
     {
       name: 'setup',
